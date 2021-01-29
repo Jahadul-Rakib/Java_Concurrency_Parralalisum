ddfc482947dbd734c9f533b6e29b5e2dc937f735
rakib <jhadul.islam@itconquest.com>
2021-01-29 12:18:24 +0600

    Add parallelism Example

--- src/main/java/com/rakib/java_concurrency/JavaConcurrencyApplication.java
+++ src/main/java/com/rakib/java_concurrency/JavaConcurrencyApplication.java
@@ -3,11 +3,22 @@ package com.rakib.java_concurrency;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class JavaConcurrencyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaConcurrencyApplication.class, args);
+
+        List<String> nameList = List.of("Rakib", "Tariqual", "Forida", "Tuli", "Dilruba");
+        List<String> stringList = nameList.parallelStream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+        System.out.println(nameList);
+        System.out.println("----------");
+        System.out.println(stringList);
     }
 
 }
