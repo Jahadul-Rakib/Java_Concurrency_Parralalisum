6ede1e89397c45506953d5f7536bcd0da99a3d72
rakib <jhadul.islam@itconquest.com>
2021-01-29 11:49:03 +0600

    add

---
+++ src/test/java/com/rakib/java_concurrency/JavaConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakib.java_concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
