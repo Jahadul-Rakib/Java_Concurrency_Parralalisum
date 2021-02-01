601baa6b1f7dd62a29bed43022eea114d162535b
rakib <jhadul.islam@itconquest.com>
2021-02-01 22:34:31 +0600

    add

--- src/main/java/com/rakib/java_concurrency/utils/CommonUtils.java
+++ src/main/java/com/rakib/java_concurrency/utils/CommonUtils.java
@@ -4,7 +4,18 @@ import org.apache.commons.lang3.time.StopWatch;
 
 public class CommonUtils {
     public static StopWatch stopWatch = new StopWatch();
-    public static void delay(long ms){
+
+    public static void startTimer() {
+        stopWatch.start();
+    }
+
+    public static void takeTime() {
+        stopWatch.stop();
+        System.out.println("Total Time Taken:    " + stopWatch.getTime());
+        stopWatch.reset();
+    }
+
+    public static void delay(long ms) {
         try {
             Thread.sleep(ms);
         } catch (InterruptedException e) {
