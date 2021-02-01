ecc9b99bb49991592c2eac805a5ad8881555aac2
rakib <jhadul.islam@itconquest.com>
2021-02-01 16:37:37 +0600

    add

--- src/main/java/com/rakib/java_concurrency/service/ProductService.java
+++ src/main/java/com/rakib/java_concurrency/service/ProductService.java
@@ -10,21 +10,65 @@ import org.springframework.stereotype.Service;
 public class ProductService {
     private final ProductInfoService productInfoService;
     private final ProductReviewService productReviewService;
+    ProductInfo productInfo;
+    ProductReview productReview;
 
     public ProductService(ProductInfoService productInfoService, ProductReviewService productReviewService) {
         this.productInfoService = productInfoService;
         this.productReviewService = productReviewService;
     }
 
-    public Product retriveProductDetails(Integer productId) {
+    public Product retriveProductDetails(Integer productId) throws InterruptedException {
         CommonUtils.stopWatch.start();
-        ProductInfo productInfo = productInfoService.retriveProductInfo(productId);
-        ProductReview productReview = productReviewService.retriveProductReview(productId);
+
+//        Runnable runnable1 = () -> productInfo = productInfoService.retriveProductInfo(productId);
+//        Runnable runnable2 = () -> productReview = productReviewService.retriveProductReview(productId);
+//        runnable1.run();
+//        runnable2.run();
+
+        ProductInfoRunnable productInfoData = new ProductInfoRunnable(productId);
+        Thread productInfoThread = new Thread(productInfoData);
+        productInfoThread.start();
+
+
+        ProductReviewRunnable productReviewData = new ProductReviewRunnable(productId);
+        Thread productReviewThread = new Thread(productReviewData);
+        productReviewThread.start();
+
+        productInfoThread.join();
+        productReviewThread.join();
+
         CommonUtils.stopWatch.stop();
         System.out.println(CommonUtils.stopWatch.getTime());
         return Product.builder()
                 .productInfo(productInfo)
                 .productReview(productReview)
                 .build();
+    }
+
+    private class ProductInfoRunnable implements Runnable {
+        public Integer productId;
+
+        public ProductInfoRunnable(Integer productId) {
+            this.productId = productId;
+        }
+
+        @Override
+        public void run() {
+            productInfo = productInfoService.retriveProductInfo(productId);
+        }
+    }
+
+    private class ProductReviewRunnable implements Runnable {
+        public Integer productId;
+
+        public ProductReviewRunnable(Integer productId) {
+            this.productId = productId;
+        }
+
+        @Override
+        public void run() {
+            productReview = productReviewService.retriveProductReview(productId);
+        }
     }
 }
